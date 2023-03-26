9e95f0dee585205847a1e2b9300974a92352f572
Roman <romanbezh@gmail.com>
2023-03-26 20:03:47 +0300

    nothing do

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -126,13 +126,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru-RU'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
-
-USE_L10N = True
 
 USE_TZ = True
 
