d44128bca7eb139545467f60929ec72556febf9b
Roman <romanbezh@gmail.com>
2023-03-26 19:49:21 +0300

    old version in hexlet

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=os.getenv('DATABASE_URL'),
         conn_max_age=600,
-        conn_health_checks=True,
+        # conn_health_checks=True,
     )
 }
 
